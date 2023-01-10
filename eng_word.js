5471eed07c3cf03e1698f4fd41a473091d6fb009
Huseyinarkin <99.arkn@gmail.com>
2023-01-11 00:14:36 +0300

    first commit

---
+++ eng_word.js
@@ -0,0 +1,101 @@
+
+
+let eng_word = [
+    {
+        word: "about",
+        mean: [
+            {
+                trWord:"hakkında",                
+                
+                exmEng:"We talked about our plans.",
+                exmTr:"Planlarımız hakkında konuştuk."
+            },
+            {
+                trWord:"yaklaşık, aşağı yukarı",                
+                
+                exmEng:"The nearest town is about five miles away.",
+                exmTr:"En yakın kasaba yaklaşık beş mil uzaklıktadır."
+            },
+        ]
+    },
+    {
+        word: "act",
+        mean: [
+            {
+                trWord:"harekete geçmek",                
+                
+                exmEng:"they urged Washington to act",
+                exmTr:"Washington'u harekete geçmeye çağırdılar"
+            },
+            {
+                trWord:"davranmak",                
+                
+                exmEng:"He acted foolishly at the meeting.",
+                exmTr:"Toplantıda aptalca davrandı."
+            },
+        ]
+    },
+    {
+        word: "actually",
+        mean: [
+            {
+                trWord:"gerçekten",                
+                
+                exmEng:"He didn't actually say anything important.",
+                exmTr:"Aslında önemli bir şey söylemedi."
+            },
+            {
+                trWord:"aslında",                
+                
+                exmEng:"he actually expected me to be pleased about it!",
+                exmTr:"aslında bundan memnun olmamı bekliyordu!"
+            },
+        ]
+    },
+    {
+        word: "by",
+        mean: [
+            {
+                trWord:"tarafından",
+            },
+            {
+                trWord:"ile, yoluyla, vasıtasıyla",
+            },
+            {
+                trWord:"... dan tutarak, dokunarak; ...erek, arak",
+            },
+            {
+                trWord:"yanında, yanıbaşında",
+            },
+            {
+                trWord:"... e kadar, ... saatine/tarihine kadar",
+            },
+        ]
+    },
+    {
+        word: "tough",
+        mean: [
+            {
+                trWord:"zor",
+            },
+            {
+                trWord:"sert, katı",
+            },
+            {
+                trWord:"güçlü, dayanıklı, sağlam",
+            },
+        ]
+    },
+    {
+        word: "throughout",
+        mean: []
+    },
+    {
+        word: "through",
+        mean: []
+    },
+    {
+        word: "though",
+        mean: []
+    }
+]
