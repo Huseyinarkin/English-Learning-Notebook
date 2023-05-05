e5789a2e9886b5de44a49f1dd173e4be09796b7d
Huseyinarkin <99.arkn@gmail.com>
2023-05-06 01:23:06 +0300

    navbar

--- app.js
+++ app.js
@@ -13,6 +13,15 @@ const mongoUrl = process.env.MONGO_URL;
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.get('/login', async (req,res) => {
+    res.render('login.ejs');
+})
+
+app.post('/login', async (req,res) => {
+    
+    res.render('login.ejs');
+})
+
 app.get('/editWord', async (req, res) => {
 
     mongoose.connect(mongoUrl);
